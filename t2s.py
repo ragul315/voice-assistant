d5e8c828314f4a2aa8aab5b2a33814f3487f05a0
Ragul J <raguljayaraj315@gmail.com>
2024-01-10 20:40:03 +0530

    text to speech

---
+++ t2s.py
@@ -0,0 +1,11 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.setProperty('rate', 200)  # Speed of speech
+engine.setProperty('volume', 1)  # Volume level (0.0 to 1.0)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id) 
+
+def speek(text):
+      
+    engine.say(text)
+    engine.runAndWait()
